0b7a728fd9a051e168a479b5e5325e30361ed50f
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2021-12-05 19:54:58 -0300

    chore: sensible data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,12 @@
-import React, { createContext, ReactNode, useContext, useState } from 'react'
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useState
+} from 'react'
+
+const { CLIENT_ID } = process.env
+const { REDIRECT_URI } = process.env
 
 import * as AuthSession from 'expo-auth-session'
 
@@ -33,15 +41,13 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
   const signInWithGoogle = async () => {
     try {
-      const CLIENT_ID = '468779211619-p6ns88of8flam04kc9uftd0hsdn3ph65.apps.googleusercontent.com'
-      const REDIRECT_URI = 'https://auth.expo.io/@rodrigomoreir/gofinances'
       const RESPONSE_TYPE = 'token'
       const SCOPE = encodeURI('profile email')
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
 
       const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse
-      // console.log(params)
+      console.log(params)
 
       if (type === 'success') {
         const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`)
